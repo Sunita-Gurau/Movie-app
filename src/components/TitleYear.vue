9e740dc671d6da586f3fef1d39847eba0d8e03a4
Sunita Gurau <sunita.gurau@introcept.co>
2022-07-18 13:39:14 +0545

    change in responsiveness

--- src/components/TitleYear.vue
+++ src/components/TitleYear.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pl-2">
-    <p class="text-white text-lg font-bold truncate">
+    <p class="text-white md:text-lg sm:text-xs font-bold truncate">
       <slot name="Title"></slot>
     </p>
     <p class="text-neutral-300 text-sm"><slot name="Year"></slot></p>
