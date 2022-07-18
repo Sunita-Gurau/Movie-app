723da3797cf42689af4b9442bd3c358992ceb6cb
Sunita Gurau <sunita.gurau@introcept.co>
2022-07-18 12:05:45 +0545

    overide tailwind class

--- src/components/VSPagination.vue
+++ src/components/VSPagination.vue
@@ -14,7 +14,7 @@
             'pagination-button',
             pageNo + index == $route.query.page ? 'active' : '',
           ]"
-          class="px-8 py-3 bg-black border-[1px] border-white px-3 py-2 font-bold text-base text-white mr-2 rounded cursor"
+          class="active:!bg-green-500 px-8 py-3 bg-black border-[1px] border-white px-3 py-2 font-bold text-base text-white mr-2 rounded cursor"
           @click="increase(pageNo + index)"
         >
           {{ pageNo + index }}
